1c2e482deac8c9bce3acbaf145c04ba7f9d2e666
223880 <rafael.segala45@gmail.com>
2024-11-26 06:48:22 -0300

    Fix

--- packages/contracts/contracts/Integrations/LUSDUsdToLUSDEth.sol
+++ packages/contracts/contracts/Integrations/LUSDUsdToLUSDEth.sol
@@ -11,6 +11,8 @@ contract LUSDUsdToLUSDEth is IPriceFeed {
     IPriceFeed public constant LUSD_USD = IPriceFeed(0x3D7aE7E594f2f2091Ad8798313450130d0Aba3a0);
     IPriceFeed public constant ETH_USD = IPriceFeed(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
     IPriceFeed public constant LUSD_ETH = IPriceFeed(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+    IPriceFeed public constant ETH_USD_ARB = IPriceFeed(0x1b41570000000000000000000000000000000000);
+    IPriceFeed public constant LUSD_ETH_BASE = IPriceFeed(0x1b41570000000000000000000000000000000000);
 
     constructor() public {}
 
